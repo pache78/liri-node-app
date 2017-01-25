f783705905bf4516070c06a3eecefb54effe4edc
pache78 <jmpacheco78@gmail.com>
2017-01-24 19:34:27 -0600

    WK10 initial

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<M1LSwM1M7UR2lujPZ15PrjqYr>',
+  consumer_secret: '<RZHWUC4T5wszCKt2EYwcr76gGNVqr9zypBzAlXZkE1PpHtEHUm>',
+  access_token_key: '<824053535331545090-NgKqu8sOWzvtMzghvCJyzChuMC21kuS>',
+  access_token_secret: '<CTLzWN00YotO27jU8c6ohYe5lw4jyA1ILRxeoRDN4C2lM>',
+}
---
+++ liri.js
@@ -0,0 +1,12 @@
+//Pkgs to be used
+
+var request = require('request');
+var twitter = require('twitter');
+var spotify = require('spotify');
+var datakeys = require('./keys.js');
+var fs = require('fs');
+
+//console.log keys
+var result = process.argv[2];
+
+console.log(result);
